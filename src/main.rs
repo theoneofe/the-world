8eafd233d5524e0bb8153d4b226724187592b00b
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-05-07 21:02:37 +0100

    Added initial everything and whatever
    I cargo init'd the directory and edited main.rs, to include the required text. Amber can do the rest of it herself.

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("The world is unstable and cannot continue.");
+    println!("Safe Mode - 1");
+    println!("Safe Mode with Space Travel - 2");
+    println!("Safe Mode with Fried Dough - 3");
+}
